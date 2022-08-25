b2e1f58538fca8efc77f1d007bb2257c8a8f86ce
Harun Akgul <46harunakgul@gmail.com>
2022-08-26 00:07:46 +0300

    Fixed Click and Keyboard Parts.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Click Button Section
+// Detecting Button Press
 
 let numberOfButtons = document.querySelectorAll(".drum").length;
 
@@ -8,47 +8,58 @@ for(let i = 0; i < numberOfButtons; i++){
         // Adding Voice Section
 
         let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
         
-        switch (buttonInnerHTML) {
-            case "w":
-                let crash = new Audio('sounds/crash.mp3');
-                crash.play(); 
-                break;
-
-                case "a":
-                let kick = new Audio('sounds/kick-bass.mp3');
-                kick.play(); 
-                break;
-
-                case "s":
-                let snare = new Audio('sounds/snare.mp3');
-                snare.play(); 
-                break;
-
-                case "d":
-                let tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play(); 
-                break;
-
-                case "j":
-                let tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play(); 
-                break;
-
-                case "k":
-                let tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play(); 
-                break;
-
-                case "l":
-                let tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play(); 
-                break;
-        
-            default: console.log(buttonInnerHTML);
-                
-        }
     });
+}
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+});
+
+// Detecting Keyboard Press
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play(); 
+            break;
+
+            case "a":
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play(); 
+            break;
+
+            case "s":
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play(); 
+            break;
+
+            case "d":
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play(); 
+            break;
+
+            case "j":
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play(); 
+            break;
+
+            case "k":
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play(); 
+            break;
+
+            case "l":
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play(); 
+            break;
+    
+        default: console.log(buttonInnerHTML);
+            
+    }
 }
 
 
